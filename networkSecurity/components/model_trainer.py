05455d5a11638a84b998b7ae5192d71bd7f63f3a
Ankit yadav <ankityaduvansh1rao9818@gmail.com>
2025-12-09 17:39:05 +0530

    deploy attempt5

--- networkSecurity/components/model_trainer.py
+++ networkSecurity/components/model_trainer.py
@@ -32,7 +32,7 @@ os.environ["MLFLOW_TRACKING_URI"] = "https://dagshub.com/ankityadavdm/ML_Network
 os.environ["MLFLOW_TRACKING_USERNAME"] = "ankityadavdm"
 os.environ["MLFLOW_TRACKING_PASSWORD"] = "<be32e038d689637213b9e45628db9dc77b21ade0>"
 # be32e038d689637213b9e45628db9dc77b21ade0
-# runmlflowtoken
+# runmlflowtoken 
 # 2ef9dc84997807e30e446d81563efe2eec3e4489
 # e4d0b5b2adac39bdb5bc9ac520d96db2aad7e342
 
